de668733177df08bd6ed5220c5346afe84005cca
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-08-07 15:33:26 +0700

    Initial commit

---
+++ app/src/main/java/com/rujirakongsomran/jc_jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.rujirakongsomran.jc_jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rujirakongsomran.jc_jetpackcomposetutorial.ui.theme.JC_JetpackComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JC_JetpackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JC_JetpackComposeTutorialTheme {
+        Greeting("Android")
+    }
+}
